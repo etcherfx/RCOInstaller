170694d0e0633942244051b12e2fcbc29aa2bbfa
EtcherFX <96310503+etcherfx@users.noreply.github.com>
2023-06-08 16:11:46 -0700

    feat: Add uninstall script
    Signed-off-by: Etcher <96310503+etcherfx@users.noreply.github.com>

---
+++ uninstall.ps1
@@ -0,0 +1,19 @@
+$versionsFolder = Join-Path $env:LOCALAPPDATA "Roblox\Versions"
+$latestVersionFolder = Get-ChildItem $versionsFolder | Where-Object { (Join-Path $_.FullName "RobloxPlayerBeta.exe") | Test-Path } | Sort-Object LastWriteTime -Descending | Select-Object -First 1
+
+if ($null -eq $latestVersionFolder) {
+    Write-Output "[Error] Could not locate the Roblox installation folder."
+}
+else {
+    $clientSettingsFolder = Join-Path $latestVersionFolder.FullName "ClientSettings"
+    if (Test-Path $clientSettingsFolder) {
+        Remove-Item -Recurse -Force $clientSettingsFolder
+        Write-Output "[Success] RCO uninstalled."
+    }
+    else {
+        Write-Output "[Error] RCO is not installed."
+    }
+}
+
+Write-Output "Press any key to exit..."
+$null = $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
